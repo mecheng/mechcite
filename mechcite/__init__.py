bde4dfc8226e76f7fc6d3200e435bd5d59e7f4e1
Jelle Spijker <j.spijker@ultimaker.com>
2019-06-01 10:38:34 +0200

    Bump version: 0.2.3 → 0.2.4

--- mechcite/__init__.py
+++ mechcite/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Jelle Spijker"""
 __email__ = 'spijker.jelle@gmail.com'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from mechcite.bibliography import Bibliography, bib
 from mechcite.citation import cite
